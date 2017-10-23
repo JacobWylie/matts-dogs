cc62b6660acddcc5c951ba171e94788539f634c3
JacobWylie <jacobwylie@gmail.com>
2017-10-23 18:24:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.btn');
+const photo = document.querySelector('#photo');
+
+btn.addEventListener('click', function() {
+	const xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === 4) {
+			let url = JSON.parse(xhr.responseText);
+			photo.setAttribute('src', url.message);
+		}
+	}
+
+	xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+	xhr.send();
+})
+
+const heading = document.querySelector('#heading');
+
+const textShine = () => {
+	let loopText = "Welcome To Random Dog Pictures"
+	for(i=0;i<loopText.length;i++) {
+		let iDiv = document.createElement('button');
+	    iDiv.classList.add("title");
+	    setInterval( () => {
+	    	iDiv.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+	    	iDiv.style.color = '#'+Math.random().toString(16).substr(-6);
+	    }, 300);
+	    iDiv.innerHTML = `${loopText[i]}  `;
+	    heading.appendChild(iDiv); 
+	}
+}
+
+textShine();
